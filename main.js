f47e68492e488963c0033ab83be635beaf3413ab
Kalutsky Sergey <SKalutsky@optimum.businesscar.ru>
2021-04-27 10:37:05 +0300

    tested some things on windows

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
+
+
+var spawn = require('child_process').spawn,
+  ls = spawn('git', ['clone', '']);
+
+ls.stdout.on('data', function (data) {
+  console.log('stdout: ' + data.toString());
+});
+
+ls.stderr.on('data', function (data) {
+  console.log('stderr: ' + data.toString());
+});
+
+ls.on('exit', function (code) {
+  console.log('child process exited with code ' + code.toString());
+});
+
+// const testFolder = '.';
+// const fs = require('fs');
+
+// fs.readdir(testFolder, (err, files) => {
+//   files.forEach(file => {
+//     console.log(file);
+//   });
+// });
+
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -11,6 +37,7 @@ function createWindow () {
   })
 
   win.loadFile('index.html')
+
 }
 
 app.whenReady().then(() => {
