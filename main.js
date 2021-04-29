c36baa76429f6bce65e92512f51e888d6910d827
Kalutsky Sergey <SKalutsky@optimum.businesscar.ru>
2021-04-29 15:52:21 +0300

    some changes

--- main.js
+++ main.js
@@ -3,30 +3,6 @@ const path = require('path')
 
 
 
-var spawn = require('child_process').spawn,
-  ls = spawn('choco', ['install', '-y', 'golang']);
-
-ls.stdout.on('data', function (data) {
-  console.log('stdout: ' + data.toString());
-});
-
-ls.stderr.on('data', function (data) {
-  console.log('stderr: ' + data.toString());
-});
-
-ls.on('exit', function (code) {
-  console.log('child process exited with code ' + code.toString());
-});
-
-// const testFolder = '.';
-// const fs = require('fs');
-
-// fs.readdir(testFolder, (err, files) => {
-//   files.forEach(file => {
-//     console.log(file);
-//   });
-// });
-
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
