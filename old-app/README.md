2954981bf64565ca96ecb323e962406e52ec0e6d
sergey <skalutsky@gmail.com>
2021-04-30 16:03:48 +0300

    understand logic of transmition and moved to separate folder

---
+++ old-app/README.md
@@ -0,0 +1,16 @@
+# bridge
+Bridge is a crossplatform desktop app for easy package/language/IDE installation, code excecution and version control.
+The goal is to make teaching code online not as good as in person, but better.
+
+Bridge is build by teacher for teachers.
+Now in its very early stage of development
+
+Technologies:
+
+Desktop app: Electron 
+Windows package manager: Chocolotey
+Unix system package manager(Linux/Mac OS): Brew
+REST API: Fast API
+Fronted: React
+version control: git, gh(cli for github)
+
---
+++ old-app/preload.js
@@ -0,0 +1,17 @@
+const { ipcRenderer } = require('electron')
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
+
+ipcRenderer.on('ping', (event, message) => {
+  const element = document.getElementById('command-text')
+  element.innerText = message 
+})
