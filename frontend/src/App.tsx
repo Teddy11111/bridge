2a5bcb7f2a0532e3810f9237564744071183c63b
sergey <skalutsky@gmail.com>
2021-05-05 14:07:49 +0300

    main areas splited

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,35 @@
 import React, {useState, useEffect} from 'react';
-
+import './App.css'
 
 const Console = () => {
   // Объявление новой переменной состояния «count»
   const [output, setCount] = useState('');
 
   return (
-    <div>
-      <p>{output}</p>
-      <button>
-        Press me
-      </button>
+    <div className='console'>
+      <p>
+      </p>
     </div>
   );
 }
 
+const DirTree = () => {
+
+  return (
+    <>
+    <div className='left-side-bar'>
+    </div>
+    <div className='workspace'>
+    <Console />
+    </div>
+    </>
+    
+  )
+
+}
+
 export default function App() {
   return (
-    <Console />
+    <DirTree />
   );
 }
